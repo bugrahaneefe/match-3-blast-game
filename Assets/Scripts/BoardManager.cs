0769d579ec856d3378dce2e392e2a023da70d00b
bugrahaneefe <bugrahaneefe@hotmail.com>
2025-02-03 16:15:36 +0300

    ui improvements

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Tilemap m_Tilemap;
+    public int Width;
+    public int Height;
+    public Tile[] Blocks;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   // Start is called before the first frame update
+   void Start()
+   {
+       m_Tilemap = GetComponentInChildren<Tilemap>();
+
+       for (int y = 0; y < Height; ++y)
+       {
+           for(int x = 0; x < Width; ++x)
+           {
+               int tileNumber = Random.Range(0, Blocks.Length);
+               //m_Tilemap.SetTile(new Vector3Int(x, y, 0), Blocks[tileNumber]);
+           }
+       }
+   }
 }
